fab5cbafc2640e94654a503ef963d6ed5ba77ad9
jmSNU <jmleeluck@naver.com>
2023-11-10 16:56:25 +0900

    AI_HW3_1110

--- HW3/knn.py
+++ HW3/knn.py
@@ -27,10 +27,26 @@ def predict_knn(x, inputs, labels, k_neighbors):
     predicted_label = 0
     ########
     # TO DO:
+    try: # Due to the typo in the load_knn_data(), I couldn't help but add this exception
+        N = inputs.shape[0]
+    except:
+        N = inputs[0].shape[0]
+        inputs = inputs[0]
+        labels = labels[0]
+    dist_arr = np.zeros((N,))
+    for i in range(0,N):
+        dist_arr[i] = np.sum((x - inputs[i])**2) # for the performance I used square of the L2 Norm
+        
+    sorted_idx = sorted(list(range(len(dist_arr))),key = lambda k:dist_arr[k],reverse=False) # sorting the index w.r.t the distances
+   
+    neighbors = sorted_idx[:k_neighbors]
+    label_of_neighbor = [labels[k] for k in neighbors] # label of the close features
 
-
+    neighbor_labels,cnt = np.unique(label_of_neighbor,return_counts=True) # cnt has the number of each label
+    predicted_label = neighbor_labels[np.argmax(cnt)] # get the maximum of cnt. Due to the characteristic of np.argmax, if the tie happened, it will return the foremost label
     ########
     return predicted_label
+
 
 
 '''
@@ -51,8 +67,22 @@ def eval_knn(inputs, labels, train_inputs, train_labels, k_neighbors):
     accuracy = 0
     ########
     # TO DO:
-
-
+    try:
+        N = inputs.shape[0]
+    except:
+        N = inputs[0].shape[0]
+        inputs = inputs[0]
+        labels = labels[0]
+        train_inputs = train_inputs[0]
+        train_labels = train_labels[0]
+    num_correct = 0
+    for i in range(N):
+        x = inputs[i] # target
+        x_label = labels[i] # true label
+        x_label_predicted = predict_knn(x,train_inputs,train_labels,k_neighbors) # predicted label
+        if x_label_predicted == x_label:
+            num_correct+=1
+    accuracy = num_correct/N
     ########
     return accuracy
 
@@ -76,8 +106,24 @@ def cross_validation_knn(k_folds, hyperparameters, inputs, labels):
     accuracies = np.zeros(len(hyperparameters))
     ########
     # TO DO:
-
-
+    try:
+        N = inputs.shape[0]
+    except:
+        N = inputs[0].shape[0]
+        inputs = inputs[0]
+        labels = labels[0]
+    for j in range(len(hyperparameters)):
+        tmp_acc = np.zeros(k_folds) 
+        for i in range(k_folds):
+            valid_sets = inputs[i*N//k_folds:(i+1) * N//k_folds] # validation set is a portion of train set
+            valid_label_sets = labels[i*N//k_folds:(i+1) * N//k_folds] # the label of validation set
+            input_sets = np.concatenate((inputs[:i*N//k_folds],inputs[(i+1)*N//k_folds:]),axis = 0) # the remaining data is regarded as train set
+            input_label_sets = np.concatenate((labels[:i*N//k_folds],labels[(i+1)*N//k_folds:]),axis = 0) # the label of train set
+            tmp_acc[i] = eval_knn(valid_sets,valid_label_sets,input_sets,input_label_sets,hyperparameters[j]) # k_folds number of accuracies must be calculated
+        accuracies[j] = np.mean(tmp_acc) # in cross_validation method, the accuracy of each hyperparameter is obtained by the average of k_folds number of accuracies(From what I've looked for, one can use the other index of estimation and representative value. However, I just used very naive one)
+    idx = np.argmax(accuracies)
+    best_accuracy = accuracies[idx]
+    best_hyperparam = hyperparameters[idx]
     ########
     return best_hyperparam, best_accuracy, accuracies
 
@@ -94,13 +140,10 @@ def plot_knn_accuracies(accuracies, hyperparams):
     plt.ylabel('accuracy')
     plt.xlabel('k neighbors')
     plt.show()
-
-
+    
 def main():
     # load data
-    train_inputs, train_labels, test_inputs, test_labels = load_knn_data()
-    print(train_inputs)
-    
+    train_inputs, train_labels, test_inputs, test_labels = load_knn_data()    
     # number of neighbors to be evaluated by cross validation
     hyperparams = range(1,31)
     k_folds = 10
@@ -116,7 +159,6 @@ def main():
     # evaluate with best # of neighbors
     accuracy = eval_knn(test_inputs, test_labels, train_inputs, train_labels, best_k_neighbors)
     print('test accuracy: '+ str(accuracy))
-
 
 if __name__ == "__main__":
     main()
